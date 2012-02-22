72bfa5e7f8d60c3a7a91aa775279618a8da84806
renner <renner.nh@gmail.com>
2012-02-22 13:51:40 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poker::Application.config.secret_token = '35ad035bc34b0bf9fc1a973553389f84740aebc30260bef22a1df66dd4d8c076b54fbe5ddb4f39353aaf14d17519c00cafb267ceb4ab3ab03a6b00a0d88da941'
